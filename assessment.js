0e363fda53318ff5cd1605401f519efd616bfad1
coloradosas <rhiannon@riselms.com>
2018-10-31 11:13:32 -0600

    completed basecamp assessment i

--- assessment.js
+++ assessment.js
@@ -1,31 +1,59 @@
 // #1 create a variable called 'myName' and assign it to your name as a string
 
+var myName =("Rhiannon");
+console.log(myName);
 // CODE HERE
 
 // #2 create a variable called 'faveNum' and assign it to your favorite number
 
-// CODE HERE
+var faveNum =30;
+console.log(faveNum);// CODE HERE
 
 // #3 create a variable called 'lovesCode' and set it to true
 
-// CODE HERE
+var lovesCode=true;
+console.log(lovesCode);// CODE HERE
 
 // #4 Using the previously created variables, create a new variable called 'me' that uses the myName and faveNum varible to create a sentence like this 'My name is Bryan and my favorite number is 3'
 
+var me = "My name is" + myName + "and my favorite number is " + faveNum;
+console.log(me);
 // CODE HERE
 
 // #5 Using the previously created faveNum variable, create a new variable called 'trueFaveNum'. *trueFaveNum* should equal true when comparing faveNum to your favorite number.v // HINT: Use a comparison operator
+
+var trueFaveNum = function() {
+  if (30 === faveNum) {return true;}
+  else {return false;}
+}
+trueFaveNum(30);
+console.log (faveNum);
 
 // CODE HERE
 
 // #6 Create a new variable called notHakeem. Using the previously created myName variable, test that your name DOES NOT equal the string 'Hakeem Olajuwon'. If your name does happen to be Hakeem Olajuwon, test against 'Karl Malone'
 
-// CODE HERE
+var myName = "Rhiannon";
+
+var notHakeem = 'Hakeem Olajuwon';
+if (myName != notHakeem)
+{
+  console.log(true);
+}
+else
+{
+  console.log(false);
+}// CODE HERE
 
 // #7 Create an if statement that checks the age variable below. If age is greater than 16, then set canDrive to true otherwise set canDrive to false. The canDrive variable will be set by the unit test so do not adjust it.
 var age = 18;
 var canDrive;
-
+function canDrive() {
+  if (age>16){
+    return true;
+  } else { return false;}
+}
+console.log(canDrive (18));
 
 // CODE HERE
 
@@ -34,13 +62,32 @@ var canDrive;
 var colorResponse;
 
 var responseCreator = function(param){
-  // CODE HERE
+  if (param = "green"){
+    colorResponse = "Green is okay";
+  }
+  else if (param = "red"){
+    colorResponse = "I heart red";
+  }
+  else {
+    colorResponse = "What is your favorite color?";
+  }
+  return colorResponse 
 }
+responseCreator ('green');
+console.log (colorResponse);
 
 // #9 Create a function called 'sum' that takes in one parameter and returns the sum of that parameter added it itself.
 
-// CODE HERE
+ function add(num1,num2) {
+   return num1 + num2; };
+   console.log(add(5,2));
+   // CODE HERE
 
 // #10 Create a function called 'greeter' that takes in two parameters, name and number. Have 'greeter' return a string that says something like this 'Hi! I am Bryan and I am 28 years old" but with the appropriate values..
-
-// CODE HERE
+  
+  function greeter(name,age){
+    var myName = 'Bryan'
+    var myAge = 28
+    return 'Hi! I am ' + myName + ' and I am ' + myAge + ' years old'
+  }
+  console.log (greeter(name,age));
